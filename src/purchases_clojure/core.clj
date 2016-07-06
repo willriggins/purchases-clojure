071455dfb8f814a1d5a0fd1600cc4c77a52970bc
willriggins <williamriggins2013@gmail.com>
2016-07-06 10:28:51 -0400

    stuff from class

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -3,6 +3,7 @@
   (:gen-class))
 
 (defn -main []
+  (println "Select a category: Furniture, Alcohol, Toiletries, Shoes, Food, or Jewelry")
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
         purchases (map (fn [line] (str/split line #","))
@@ -10,9 +11,12 @@
         header (first purchases)
         purchases (rest purchases)
         purchases (map (fn [line] (zipmap header line))
-                    purchases)]    
-    purchases))
-    
+                    purchases)
+        input (read-line)
+        purchases (filter #(= input (get % "category"))
+                    purchases)
+        text (pr-str purchases)]
+    (spit "filtered_purchases.edn" text))) 
    
        
     
